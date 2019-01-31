f991dd7824b0dfbd34f9de830844d95904733473
Andrew Ribeiro <andtankian@live.com>
2019-01-31 21:56:06 -0200

    Publishing distribution.

---
+++ dist/Style.js
@@ -0,0 +1,190 @@
+import _taggedTemplateLiteral from "@babel/runtime/helpers/esm/taggedTemplateLiteral";
+
+function _templateObject14() {
+  var data = _taggedTemplateLiteral(["\n  padding: 10px;\n  padding-top: 20px;\n  overflow-y: scroll;\n  max-height: calc(100vh - 30px);\n"]);
+
+  _templateObject14 = function _templateObject14() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject13() {
+  var data = _taggedTemplateLiteral(["\npadding: 0;\nmargin: 0;\nheight: 20px;\nborder: 0;\nbox-shadow: 0 10px 20px -10px rgba(0,0,0,0.1) inset;\n"]);
+
+  _templateObject13 = function _templateObject13() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject12() {
+  var data = _taggedTemplateLiteral(["\n  display: flex;\n  justify-content: center;\n  align-items: center;\n  padding: 0.3em 0;\n  width: ", ";\n  max-height: 50px;\n  height: 50px;\n  transition: background-color 0.3s;\n  :hover {\n    background-color: rgba(0,0,0,0.03);\n  }\n  cursor: pointer;\n"]);
+
+  _templateObject12 = function _templateObject12() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject11() {
+  var data = _taggedTemplateLiteral(["\n  font-size: .8em;\n  padding:0;\n  margin:0;\n  white-space: nowrap;\n  text-overflow: ellipsis;\n  overflow: hidden;\n  font-family: Raleway;\n"]);
+
+  _templateObject11 = function _templateObject11() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject10() {
+  var data = _taggedTemplateLiteral(["\n  padding:0;\n  margin:0;\n  font-size: 1.3em;\n  font-family: Raleway;\n  overflow: hidden;\n  white-space: nowrap;\n  text-overflow: clip;\n"]);
+
+  _templateObject10 = function _templateObject10() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject9() {
+  var data = _taggedTemplateLiteral(["\n  max-width: 100%;\n  overflow: hidden;\n  padding: 1em;\n  width: 100%;\n"]);
+
+  _templateObject9 = function _templateObject9() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject8() {
+  var data = _taggedTemplateLiteral(["\n  width: ", ";\n  padding: 0;\n  margin: 0;\n  display: flex;\n  justify-content: flex-end;\n"]);
+
+  _templateObject8 = function _templateObject8() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject7() {
+  var data = _taggedTemplateLiteral(["\n  display: flex;\n  align-items: center;\n  justify-content: ", ";\n  padding: 0.3em 0;\n  max-height: 50px;\n  height: 50px;\n  transition: background-color 0.3s;\n  :hover {\n    background-color: rgba(0,0,0,0.03);\n  }\n  cursor: pointer;\n  color: ", "\n"]);
+
+  _templateObject7 = function _templateObject7() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject6() {
+  var data = _taggedTemplateLiteral(["\n  width: 63%;\n  height: 5px;\n  margin: 4px 0;\n  background-color: rgba(84, 84, 84, 0.77);\n  transition: transform 0.2s;\n  transform-origin: left center;\n"]);
+
+  _templateObject6 = function _templateObject6() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject5() {
+  var data = _taggedTemplateLiteral(["\n  width: 60px;\n  height: 60px;\n  min-width: 60px;\n  cursor: pointer;\n  display: flex;\n  flex-direction: column;\n  align-items: center;\n  justify-content:center;\n  background-color: white;\n  box-shadow: 0 10px 25px -2px rgba(0,0,0,0.1);\n  transform-origin: left center;\n  transition: transform .3s, box-shadow .3s;\n  .top {\n    transform: ", ";\n    width: 50%;\n    margin: ", ";\n  }\n  .middle {\n    opacity: ", "\n  }\n  .bottom {\n    transform: ", ";\n    width: 50%;\n    margin: ", ";\n  }\n\n  &:hover{\n    box-shadow: -5px 5px 25px -2px rgba(0,0,0,0.1);\n    transform: perspective(20em) ", ";\n  }\n"]);
+
+  _templateObject5 = function _templateObject5() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject4() {
+  var data = _taggedTemplateLiteral(["\n  padding: 0px;\n  margin: 0px;\n  display: flex;\n"]);
+
+  _templateObject4 = function _templateObject4() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject3() {
+  var data = _taggedTemplateLiteral(["\n  box-shadow: 8px 0 30px -4px rgba(0,0,0,0.1);\n  background-color: #fff;\n  height: 100vh;\n  transition: width 0.4s cubic-bezier(.86,.47,0,1), min-width 0.4s cubic-bezier(.86,.47,0,1);\n  width: ", ";\n  min-width: ", ";\n  overflow: hidden;\n  @media (max-width: 500px) {\n    width: ", ";\n    min-width: ", ";\n  }\n"]);
+
+  _templateObject3 = function _templateObject3() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject2() {
+  var data = _taggedTemplateLiteral(["\n  @font-face {\n    font-family: Raleway;\n    src: url(", ") format(\"TrueType\");\n    font-style: light;\n    font-weight: 200;\n    font-display: fallback;\n  }\n"]);
+
+  _templateObject2 = function _templateObject2() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject() {
+  var data = _taggedTemplateLiteral(["\n  body {\n    padding:0;\n    margin:0;\n    background-color: rgba(33, 29, 29, 0.06);\n  }\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+import styled, { createGlobalStyle } from 'styled-components';
+import Raleway from './assets/fonts/Raleway-Light.ttf';
+var navbarOpenedWidthMedium = '20vw';
+var navbarOpenedWidthSmall = '80vw';
+var navbarClosedWidthMedim = '75px';
+var navbarItemTextColorActive = 'rgba(193, 7, 18, 0.86)';
+export var GlobalStyle = createGlobalStyle(_templateObject());
+export var DefaultFont = createGlobalStyle(_templateObject2(), Raleway);
+export var VerticalNavbarStyled = styled.div(_templateObject3(), function (props) {
+  return props.isShown ? navbarOpenedWidthMedium : navbarClosedWidthMedim;
+}, function (props) {
+  return props.isShown ? navbarOpenedWidthMedium : navbarClosedWidthMedim;
+}, function (props) {
+  return props.isShown ? navbarOpenedWidthSmall : '0px';
+}, function (props) {
+  return props.isShown ? navbarOpenedWidthSmall : '0px';
+});
+export var NavbarContainer = styled.div(_templateObject4());
+export var NavbarTogglerStyled = styled.div(_templateObject5(), function (props) {
+  return props.isShown ? 'translateX(5.5px) rotate(45deg)' : 'rotate(0)';
+}, function (props) {
+  return props.isShown && '1.6px';
+}, function (props) {
+  return props.isShown ? 0 : 1;
+}, function (props) {
+  return props.isShown ? 'translateX(5.5px) rotate(-45deg)' : 'rotate(0)';
+}, function (props) {
+  return props.isShown && '1.6px';
+}, function (props) {
+  return props.isShown ? 'rotateY(-25deg)' : 'rotateY(18deg)';
+});
+export var HumbBar = styled.div(_templateObject6());
+export var NavbarItemStyled = styled.div(_templateObject7(), function (props) {
+  return props.isShown ? 'flex-start' : 'center';
+}, function (props) {
+  return props.isActive ? navbarItemTextColorActive : '';
+});
+export var NavbarItemIconContainer = styled.div(_templateObject8(), function (props) {
+  return props.isShown ? '30%' : '';
+});
+export var NavbarItemTextContainer = styled.div(_templateObject9());
+export var NavbarItemTitle = styled.h1(_templateObject10());
+export var NavbarItemSubtitle = styled.h3(_templateObject11());
+export var NavBarHeaderItem = styled.header(_templateObject12(), function (props) {
+  return props.isShown ? '100%' : '75px';
+});
+export var NavbarDivider = styled.hr(_templateObject13());
+export var Content = styled.div(_templateObject14());
