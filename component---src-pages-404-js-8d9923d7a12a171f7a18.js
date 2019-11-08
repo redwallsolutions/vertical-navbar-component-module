9357b1272405db18fbcaa9a94816f926d2c931bf
Andrew Ribeiro <andtankian@gmail.com>
2019-11-08 10:38:02 -0300

    Updates

---
+++ component---src-pages-404-js-8d9923d7a12a171f7a18.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.jsx)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-8d9923d7a12a171f7a18.js.map
---
+++ styles-98a1b3538015d4964952.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{oKgo:function(o,n,w){}}]);
+//# sourceMappingURL=styles-98a1b3538015d4964952.js.map
