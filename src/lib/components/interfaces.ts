23ef4930e0c819620dd45e5f6678471b614ec651
Andrew Ribeiro <andtankian@gmail.com>
2019-11-08 10:09:27 -0300

    Improved, documentations

--- src/lib/components/interfaces.ts
+++ src/lib/components/interfaces.ts
@@ -26,11 +26,18 @@ export interface IItemProps {
 
 export interface INavbarProps extends ICommonProps {
 	/**
-	 * The items props defines all icons and items that will be displayed in along the navbar.
+	 * The items prop is an Array of  `IItemProps`  that defines all items to be displayed along the navbar.
+	 * Each item in the array consists of:
+	 * 
+	 * - `name:string`
+	 * - `icon:ReactNode`
+	 * - `handler:Function`
+	 * 
+	 * Example:  `items: [{name: 'Dashboard', icon: <DashboardIcon/>, handler: ()=>console.log('dashboard clicked')}]`
 	 */
-	items: Array<IItemProps>,
+	items: IItemProps[],
 	/**
-	 * The logo props defines a logo that will be displayed at the top of navbar. It's hidden when navbar is horizontal. (Responsive mode)
+	 * The logo prop defines an image logo that will be displayed at the top of navbar. It's hidden when navbar is horizontal. (Responsive mode)
 	 */
     logo:string
 }
